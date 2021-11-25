c989e4373cc3889cfb195d96dcc28398ada83700
Philippe Roy <renaissance542@gmail.com>
2021-11-25 13:57:51 -0500

    refactored :merge to be shorter for the merciless slave-driver Rubocop

--- merge_sort.rb
+++ merge_sort.rb
@@ -7,19 +7,10 @@ def merge_sort(arr)
   merge(merge_sort(arr[0..size / 2 -1]), merge_sort(arr[(size / 2)..size - 1]))
 end
 
-# rubocop:disable Metrics/MethodLength
 def merge(arr1, arr2)
   result = []
-  until arr1.empty? || arr2.empty?
-    if arr1[0] < arr2[0]
-      result.push(arr1.shift)
-      result.concat arr2 if arr1.empty?
-    else
-      result.push(arr2.shift)
-      result.concat arr1 if arr2.empty?
-    end
-  end
-  result
+  result.push(arr1[0] < arr2[0] ? arr1.shift : arr2.shift) until arr1.empty? || arr2.empty?
+  result.concat(arr1).concat(arr2)
 end
 
 arr1 = [4, 2, 7, 3, 9, 7, 3]
